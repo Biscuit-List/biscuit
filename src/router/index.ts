63bd7b1402c45c4a0b574e49c79618281990af91
dayeon-choi <dayeon2399@naver.com>
2021-07-21 03:31:08 +0900

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,23 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+import LandingPage from "../views/LandingPage/LandingPage.vue";
+import BucketListPage from "../views/BucketListPage/BucketListPage.vue";
 
-const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(VueRouter);
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+export const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "LandingPage",
+      component: LandingPage
+    },
+    {
+      path: "/bucket-list",
+      name: "BucketListPage",
+      component: BucketListPage
+    }
+  ]
+});
---
+++ src/views/BucketListPage/BucketListPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <div>BucketListPage</div>
+  </div>
+</template>
